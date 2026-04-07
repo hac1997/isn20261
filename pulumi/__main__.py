f6662acb71db7e30279758f9a7ef66cb371a3a0c
Ederson Torresini <etorresini@ifsc.edu.br>
2026-04-07 17:06:25 +0000

    Reorganizar arquivos (e apagar antigos)

--- pulumi/__main__.py
+++
@@ -1,8 +0,0 @@
-import pulumi
-import pulumi_aws as aws
-
-queue = aws.sqs.Queue("isn20261")
-
-pulumi.export("queue_name", queue.name)
-pulumi.export("queue_url", queue.url)
-pulumi.export("queue_arn", queue.arn)
